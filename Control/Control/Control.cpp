845098da576dafc98170cc0243e394876eb83df2
Diogo Canut <diogocanut@hotmail.com>
2020-01-28 23:35:52 -0300

    first commit: initial state

---
+++ Control/Control/Control.cpp
@@ -0,0 +1,217 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <strsafe.h>
+// Binn é a biblioteca usada para serialização dos dados
+#include "binn.h"
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+#define DEFAULT_BUFLEN 1024
+/* 
+	Portas para recebimento e envio de comandos e dados
+ O módulo de controle consiste em 2 threads de comandos, uma para recebimento do cliente
+ e outra para repasse dos comandos para o módulo de experimentos, e 2 threads de dados, recebendo 
+ dados do módulo de experimento e repassando para o cliente
+ */
+#define RECEIVE_COMMANDS_PORT "27015"
+#define SEND_COMMANDS_PORT "27013"
+#define RECEIVE_DATA_PORT "27011"
+#define SEND_DATA_PORT "27012"
+
+// Estrutura do comando
+struct Command {
+	int ack;
+	int type;
+	int command;
+};
+
+struct Command command;
+
+WSADATA wsaData;
+
+// Id das respectivas threads
+DWORD receive_commands_thread_id;
+DWORD send_commands_server_thread_id;
+DWORD receive_data_thread_id;
+DWORD send_data_thread_id;
+
+// Socket da thread de envio de comandos e dados
+SOCKET SendCommandsClientSocket = INVALID_SOCKET;
+SOCKET SendDataClientSocket = INVALID_SOCKET;
+
+// Buffers de dados globais
+char command_recvbuf[DEFAULT_BUFLEN];
+char data_recvbuf[DEFAULT_BUFLEN];
+// Declaração de cada uma das threads
+DWORD WINAPI receive_commands_thread(LPVOID lpParam);
+DWORD WINAPI send_commands_server_thread(LPVOID lpParam);
+DWORD WINAPI receive_data_thread(LPVOID lpParam);
+DWORD WINAPI send_data_thread(LPVOID lpParam);
+
+HANDLE commandsMutex;
+
+int main()
+{
+	int wsa;
+
+	wsa = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (wsa != 0) {
+		printf("WSAStartup failed with error: %d\n", wsa);
+		return 1;
+	}
+
+	commandsMutex = CreateMutex(
+		NULL,
+		FALSE,
+		NULL);
+	if (commandsMutex == NULL) {
+		printf("CreateMutex error: %d\n", GetLastError());
+		return 1;
+	}
+
+
+	HANDLE hndThread1;
+	hndThread1 = CreateThread(NULL, 0, send_commands_server_thread, NULL, 0, &receive_commands_thread_id);
+
+	if (NULL != hndThread1)
+		WaitForSingleObject(hndThread1, INFINITE);
+	CloseHandle(hndThread1);
+
+	CloseHandle(commandsMutex);
+
+	return 0;
+}
+
+DWORD WINAPI send_commands_server_thread(LPVOID lpParam) {
+	int iResult;
+
+	SOCKET ListenSocket = INVALID_SOCKET;
+
+	struct addrinfo* result = NULL;
+	struct addrinfo hints;
+
+	int iSendResult;
+
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	binn* obj;
+	obj = binn_object();
+	DWORD dwWaitResult;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	iResult = getaddrinfo(NULL, SEND_COMMANDS_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("socket failed with error: %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		printf("bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	freeaddrinfo(result);
+
+	iResult = listen(ListenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR) {
+		printf("listen failed with error: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+
+	SendCommandsClientSocket = accept(ListenSocket, NULL, NULL);
+	if (SendCommandsClientSocket == INVALID_SOCKET) {
+		printf("accept failed with error: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+
+	do {
+
+		dwWaitResult = WaitForSingleObject(
+			commandsMutex,
+			INFINITE);
+
+		switch (dwWaitResult)
+		{
+		case WAIT_OBJECT_0:
+			__try {
+
+				command.ack = 4;
+				command.type = 2;
+				command.command = 12345;
+				binn_object_set_int32(obj, (char*)"ack", command.ack);
+				binn_object_set_int32(obj, (char*)"type", command.type);
+				binn_object_set_int32(obj, (char*)"command", command.command);
+
+				iSendResult = send(SendCommandsClientSocket, (const char*)binn_ptr(obj), binn_size(obj), 0);
+				printf("Bytes sent: %d\n", iSendResult);
+			}
+			__finally {
+				if (iSendResult == SOCKET_ERROR) {
+					printf("send failed with error: %d\n", WSAGetLastError());
+					closesocket(SendCommandsClientSocket);
+					WSACleanup();
+					return 1;
+				}
+
+				if (!ReleaseMutex(commandsMutex))
+				{
+					printf("Error releasing mutex: %d\n", GetLastError());
+					return 1;
+				}
+
+			}
+			break;
+
+		case WAIT_ABANDONED:
+			return 1;
+		}
+
+	} while (iResult > 0);
+	
+	iResult = shutdown(SendCommandsClientSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(SendCommandsClientSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	WSACleanup();
+	closesocket(ListenSocket);
+	binn_free(obj);
+
+	return 0;
+}
---
+++ Experiment/Experiment/Experiment.cpp
@@ -0,0 +1,156 @@
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <strsafe.h>
+#include "binn.h"
+
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
+
+#define DEFAULT_BUFLEN 1024
+#define RECEIVE_COMMANDS_FROM_CONTROL_PORT "27013"
+#define SEND_COMMANDS_SERVER_PORT "27016"
+
+WSADATA wsaData;
+
+DWORD receive_commands_thread_id;
+DWORD send_commands_server_thread_id;
+CRITICAL_SECTION critical;
+
+SOCKET ExperimentClientSocket = INVALID_SOCKET;
+
+char ack_recvbuf[DEFAULT_BUFLEN];
+
+DWORD WINAPI receive_commands_thread(LPVOID lpParam);
+DWORD WINAPI send_commands_server_thread(LPVOID lpParam);
+
+char* args;
+
+int main(int argc, char** argv)
+{
+	int wsa;
+
+	// Initialize Winsock
+	wsa = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (wsa != 0) {
+		printf("WSAStartup failed with error: %d\n", wsa);
+		return 1;
+	}
+	/*
+	if (argc != 2) {
+		printf("usage: %s experiment-server-name\n", argv[0]);
+		return 1;
+	}
+
+	args = argv[1];
+	*/
+	HANDLE hndThread1;
+	HANDLE hndThread2;
+	hndThread1 = CreateThread(NULL, 0, receive_commands_thread, NULL, 0, &receive_commands_thread_id);
+
+	if (NULL != hndThread1)
+		WaitForSingleObject(hndThread1, INFINITE);
+	CloseHandle(hndThread1);
+
+	return 0;
+}
+
+DWORD WINAPI receive_commands_thread(LPVOID lpParam) {
+	int iResult;
+
+	SOCKET ConnectSocket = INVALID_SOCKET;
+
+	struct addrinfo* result = NULL,
+		* ptr = NULL,
+		hints;
+
+	int iSendResult;
+
+	int recvbuflen = DEFAULT_BUFLEN;
+	char sendbuf[DEFAULT_BUFLEN];
+	char recvbuf[DEFAULT_BUFLEN];
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	// Resolve the server address and port
+	iResult = getaddrinfo("localhost", RECEIVE_COMMANDS_FROM_CONTROL_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	// Attempt to connect to an address until one succeeds
+	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+		// Create a SOCKET for connecting to server
+		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+			ptr->ai_protocol);
+		if (ConnectSocket == INVALID_SOCKET) {
+			printf("socket failed with error: %ld\n", WSAGetLastError());
+			WSACleanup();
+			return 1;
+		}
+
+		// Connect to server.
+		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+		if (iResult == SOCKET_ERROR) {
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+			continue;
+		}
+		break;
+	}
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server!\n");
+		WSACleanup();
+		return 1;
+	}
+
+	binn* obj;
+
+	// Create binn object
+	obj = binn_object();
+
+	// Receive until the peer closes the connection
+		iResult = recv(ConnectSocket, (char *)binn_ptr(obj), DEFAULT_BUFLEN, 0);
+		if (iResult > 0) {
+			printf("Bytes received: %d\n", iResult);
+			int test = binn_object_int32(obj, (char*)"ack");
+			printf("Experiment server received command: %d\n", test);
+		}
+		else if (iResult == 0)
+			printf("Connection closed\n");
+		else
+			printf("recv failed with error: %d\n", WSAGetLastError());
+
+	
+	// shutdown the connection since no more data will be sent
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// cleanup
+	closesocket(ConnectSocket);
+	WSACleanup();
+
+	return 0;
+}
